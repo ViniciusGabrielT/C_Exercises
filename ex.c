5c4e0e8fcae518bc684ad91f8622108a5c94501c
Vinicius Torres <viniciusgabriellopestorres@outlook.com>
2025-08-21 23:11:40 -0300

    Realizado os exercícios de 2 ao 4.

--- ex.c
+++ ex.c
@@ -7,12 +7,23 @@ void end();
 
 void ex01();
 void ex02();
+void ex03();
+void ex04();
+float ctof(float);
+float ctok(float);
+float ftoc(float);
+float ftok(float);
+float ktoc(float);
+float ktof(float);
+void ex05();
+
+char grade = 248;
 
 int main()
 {
     int x = 0;
 
-    printf("\nHello, type the exercise to be executed (1-x): ");
+    printf("\nType the exercise to be executed (1-28): ");
     scanf("%d", &x);
 
     start();
@@ -25,8 +36,17 @@ int main()
     case 2:
         ex02();
         break;
+    case 3:
+        ex03();
+        break;
+    case 4:
+        ex04();
+        break;
+    case 5:
+        ex05();
+        break;
     default:
-        printf("Error! This exercise isn't available. Try again.");
+        printf("Error! This exercise isn't available yet. Try another one.");
         break;
     }
 
@@ -42,9 +62,7 @@ void ex01()
 {
     float kg = 0.0, m = 0.0;
 
-    printf("Welcome to the BMI calculator!");
-
-    printf("\n\nType your weight in kilograms: ");
+    printf("\nType your weight in kilograms: ");
     scanf("%f", &kg);
 
     printf("\nType your height in meters: ");
@@ -53,4 +71,88 @@ void ex01()
     printf("\nYour BMI is: %.2f", kg / (m * m));
 }
 
-void ex02() {}
+void ex02()
+{
+    float brl = 0.0, usd = 0.0;
+
+    printf("\nType the BRL ammount to be converted: R$");
+    scanf("%f", &brl);
+
+    printf("\nType the current exchange rate: US$");
+    scanf("%f", &usd);
+
+    printf("\nR$%.2f in USD is US$%.2f.", brl, brl * usd);
+}
+
+void ex03()
+{
+    int length = 0, width = 0;
+
+    printf("\nType the land length in meters: ");
+    scanf("%d", &length);
+
+    printf("\nType the land width in meters: ");
+    scanf("%d", &width);
+
+    printf("\nThe land area is %d square meters.", length * width);
+}
+
+void ex04()
+{
+    float x = 0.0, y = 0.0;
+    int option = 0;
+
+    printf("Choose the desired conversion:\n1 - Celsius to Fahrenheit;\n2 - Celsius to Kelvin;\n3 - Fahrenheit to Celsius;\n4 - Fahrenheit to Kelvin;\n5 - Kelvin to Celsius;\n6 - Kelvin to Fahrenheit.\nOption: ");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        printf("\nType the Celsius temp to be converted to Fahrenheit: ");
+        scanf("%f", &x);
+        y = ctof(x);
+        printf("\nResult: %.2f%cC = %.2f%cF", x, grade, y, grade);
+        break;
+    case 2:
+        printf("\nType the Celsius temp to be converted to Kelvin: ");
+        scanf("%f", &x);
+        y = ctok(x);
+        printf("\nResult: %.2f%cC = %.2fK", x, grade, y);
+        break;
+    case 3:
+        printf("\nType the Fahrenheit temp to be converted to Celsius: ");
+        scanf("%f", &x);
+        y = ftoc(x);
+        printf("\nResult: %.2f%cF = %.2f%cC", x, grade, y, grade);
+        break;
+    case 4:
+        printf("\nType the Fahrenheit temp to be converted to Kelvin: ");
+        scanf("%f", &x);
+        y = ftok(x);
+        printf("\nResult: %.2f%cF = %.2fK", x, grade, y);
+        break;
+    case 5:
+        printf("\nType the Kelvin temp to be converted to Celsius: ");
+        scanf("%f", &x);
+        y = ktoc(x);
+        printf("\nResult: %.2fK = %.2f%cC", x, y, grade);
+        break;
+    case 6:
+        printf("\nType the Kelvin temp to be converted to Fahrenheit: ");
+        scanf("%f", &x);
+        y = ktof(x);
+        printf("\nResult: %.2fK = %.2f%cF", x, y, grade);
+        break;
+    default:
+        printf("\nError! Option unavailable.");
+        break;
+    }
+}
+float ctof(float c) { return (c * 9 / 5) + 32; }
+float ctok(float c) { return c + 273.15; }
+float ftoc(float f) { return (f - 32) * 5 / 9; }
+float ftok(float f) { return (f - 32) * 5 / 9 + 273.15; }
+float ktoc(float k) { return k - 273.15; }
+float ktof(float k) { return (k - 273.15) * 9 / 5 + 32; }
+
+void ex05() {}
